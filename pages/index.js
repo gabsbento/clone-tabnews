70c5401079bb1d05b98769e6e73bcd141f7e5dd1
gabsbento <71283367+gabsbento@users.noreply.github.com>
2025-12-09 15:58:08 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Aquele que luta contra monstros deve ter cuidado para não se tornar um monstro. E se você olhar por tempo suficiente para o abismo, o abismo também olhará para você.</h1>;
+}
+
+export default Home;
